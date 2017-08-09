5626f85fb746bd744071025741579cd07ec56d3c
foreverwind <foreverwind229@qq.com>
2017-08-09 13:58:53 +0800

    init iOS_OC project

---
+++ iOS_OC/iOS_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_OC
+//
+//  Created by star on 2017/8/9.
+//  Copyright © 2017年 foreverwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_OC/iOS_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_OC
+//
+//  Created by star on 2017/8/9.
+//  Copyright © 2017年 foreverwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
